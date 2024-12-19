04092d27a653189a704c3282527c4e2ade944342
Aidanomalley20 <omalleyac80@gmail.com>
2024-12-19 09:14:51 -0600

    final changes

--- server.js
+++ server.js
@@ -2,31 +2,24 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
+const employeesRouter = require("./routes/employees");
+app.use("/employees", employeesRouter);
+
 app.get("/", (req, res) => {
   res.send("Hello employees!");
 });
 
-const employees = require("./employees");
-
-app.get("/employees", (req, res) => {
-  res.json(employees);
+app.use((req, res) => {
+  res.status(404).json({ error: "Not Found" });
 });
 
-app.get("/employees/random", (req, res) => {
-  const i = Math.floor(Math.random() * employees.length);
-  res.json(employees[i]);
-});
-
-app.get("/employees/:id", (req, res) => {
-  const { id } = req.params;
-  const employee = employees.find((e) => e.id === +id);
-  if (employee) {
-    res.json(employee);
-  } else {
-    res.status(404).send(`There is no employee with id ${id}.`);
-  }
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Internal Server Error" });
 });
 
 app.listen(PORT, () => {
-  `Listening on port ${PORT}...`;
+  console.log(`Listening on port ${PORT}...`);
 });
